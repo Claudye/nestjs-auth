799045a2ca5885a3f0f6590eb135bcc40ea44c91
Claudye <dev.claudy@gmail.com>
2022-11-28 09:04:57 +0100

    jwt-auth strategy

--- src/auth/strategy/jwt-auth.strategy.ts
+++ src/auth/strategy/jwt-auth.strategy.ts
@@ -2,13 +2,18 @@ import {Injectable} from '@nestjs/common';
 import {PassportStrategy} from '@nestjs/passport';
 import {Strategy} from 'passport-jwt' ;
 import {JWT_AUTH_KEY} from 'src/constants/env';
+import {UsersService} from 'src/users/users.service';
 @Injectable()
 export class JwtAuthStratey extends PassportStrategy(Strategy, 'jwt-auth') {
-	constructor(){
+	constructor(private usersService: UsersService){
 		super({
 			jwtFromRequest:
 				ExtractJwt.fromAuthHeaderAsBearerToken()
 				secretOrKey:JWT_AUTH_KEY
 		})
+	}
+
+	validate(payload: any){
+
 	}
 }
